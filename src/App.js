f19edb22ac91ab6fc2875eb8d7d66325da5606ca
Brian McPhillips <B.McPhillips89@gmail.com>
2020-08-05 22:19:40 -0700

    Page is working still needs styling and diagram

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import request from 'superagent';
+import Search from './Search.js';
+import PokeList from './PokeList.js';
+
+class App extends React.Component {
+  state = {
+    search: '',
+    isLoadig: false,
+    pokeState: []
+  }
+
+  handleClick = async () => {
+    this.setState({isLoading: true})
+    const data = await request.get(`https://alchemy-pokedex.herokuapp.com/api/pokedex?perPage=1000&pokemon=${this.state.search}`);
+    
+    this.setState({
+      pokeState: data.body.results,
+      isLoading: false,
+    })
+  }
+  handlePokemonType = (e) => {
+    const type = e.target.value;
+
+    this.setState({ search: type })
+  }
+
+  render() {
+    return (
+      <main>
+        <Search handleSearch={this.handlePokemonType} />
+        <button onClick={this.handleClick}>Search for Pokemon</button>
+        <PokeList pokemons={this.state.pokeState} />
+      </main>
+    )
+  }
 }
 
 export default App;
+
+
