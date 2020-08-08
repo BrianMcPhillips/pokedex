326bc50988a174014a42025729f4c486ef267cf8
Brian McPhillips <B.McPhillips89@gmail.com>
2020-08-08 10:14:25 -0700

    javascript complete

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -3,53 +3,110 @@ import React from 'react';
 import styles from './App.css';
 import request from 'superagent';
 //import Search from './Search.js';
-import PokeItem from './PokeItem.js';
+import PokeList from './PokeList.js';
 
 class SearchPage extends React.Component {
   state = {
     search: '',
     searchBy: 'pokemon',
     isLoadig: false,
-    pokeState: []
+    pokeState: [],
+    currentPage: 1,
+    totalPages: 1
   }
 
-  handleClick = async () => {
-    this.setState({isLoading: true})
-    const data = await request.get(`https://alchemy-pokedex.herokuapp.com/api/pokedex?perPage=1000&${this.state.searchBy}=${this.state.search}`);
+  componentDidMount = async () => {
+    const params = new URLSearchParams(this.props.location.search);
+
+    const searchBy = params.get('searchBy');
+    const page = params.get('page');
+    const search = params.get('search');
+
+    if (searchBy && page && search) {
+      await this.setState({
+        searchBy: searchBy,
+        currentPage: page,
+        search: search,
+      });
+    }
     
-    this.setState({
+    await this.makeRequest()
+  
+  }
+
+  makeRequest = async () => {
+    this.setState({ isLoading: true });
+    const data = await request.get(`https://alchemy-pokedex.herokuapp.com/api/pokedex?page=${this.state.currentPage}&perPage=20&${this.state.searchBy}=${this.state.search}`);
+    await this.setState ({
       pokeState: data.body.results,
+      totalPages: Math.ceil(data.body.count / 20),
       isLoading: false,
     })
-    console.log("hello ladies");
-  }
-  handlePokemonType = (e) => {
-    const type = e.target.value;
 
+    const params = new URLSearchParams(this.props.location.search);
+    params.set('search', this.state.search);
+    params.set('searchBy', this.state.searchBy);
+    params.set('page', this.state.currentPage);
+
+    this.props.history.push('?' + params.toString())
+  }
+
+  handleSubmit = async (e) => {
+    e.preventDefault();
+    await this.setState({
+      currentPage: 1
+    })
+    await this.makeRequest()
+  }
+
+  handleNextClick = async () => {
+    await this.setState({ currentPage: Number(this.state.currentPage) + 1 })
+    
+    await this.makeRequest();
+  }
+
+  handlePrevClick = async () => {
+    await this.setState({ currentPage: Number(this.state.currentPage) - 1 })
+    
+    await this.makeRequest();
+  }
+    
+  handleDogType = (e) => {
+    const type = e.target.value;
     this.setState({ filter: type })
   }
 
   render() {
-    const { isLoading, pokeState } = this.state;
+    const { 
+      isLoading, 
+      pokeState,
+      currentPage,
+      totalPages 
+  } = this.state;
     return (
       <main className="main">
         <div>
-          <input onChange={(e) => this.setState({ search: e.target.value })} />
-          <select onChange={(e) => { this.setState({ searchBy: e.target.value })} }>
+          <form onSubmit={this.handleSubmit}>
+          <input onChange={(e) => this.setState({ search: e.target.value })} value={this.state.search}/>
+          <select onChange={(e) => { this.setState({ searchBy: e.target.value })} } value={this.state.searchBy}>
             <option value='pokemon'>name</option>
             <option value='type'>type</option>
             <option value='attack'>attack</option>
             <option value='defense'>defense</option>
           </select>
           <button onClick={this.handleClick}>Get Pokemon</button>
-            
+          </form>
         </div>
         <div className='poke-display'>
         {
             isLoading
               ? <p className={styles.spin}></p>
-              : pokeState.map(poke => <PokeItem key={poke.id} pokemon={poke} />)
-            }
+              : <PokeList 
+                handleNextClick={this.handleNextClick} handlePrevClick={this.handlePrevClick} 
+                currentPage={currentPage}
+                pokeState={pokeState}
+                totalPages={totalPages} />
+          }
         </div>
         </main>
     );
